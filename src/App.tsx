e90932b20fd3928a1989d38e6fe0defbf5ad97c8
eMartinPrieto <eduardomartinprieto@gmail.com>
2026-04-28 00:54:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LessonPage from './pages/LessonPage'
 import AppShell from './components/AppShell'
+import ScrollToTop from './components/ScrollToTop'
 
 export default function App() {
   return (
     <AppShell>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/lesson/:lessonId" element={<LessonPage />} />
